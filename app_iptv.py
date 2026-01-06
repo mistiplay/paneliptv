95f28cdc42e22a4b160618500f11ec9185125bb8
mistiplay <rrrichard.g92@gmail.com>
2026-01-06 12:42:52 -0500

    Update app_iptv.py

--- app_iptv.py
+++ app_iptv.py
@@ -81,13 +81,13 @@ st.markdown("""
     /* Imagen Ratio 2:3 */
     .vod-img-box {
         width: 100%;
-        padding-top: 50%; 
+        padding-top: 100%; 
         position: relative;
     }
     .vod-img {
         position: absolute;
         top: 0; left: 0; bottom: 0; right: 0;
-        width: 100%; height: 100%;
+        width: 100%; height: 80%;
         object-fit: cover;
     }
     /* Info Compacta */
